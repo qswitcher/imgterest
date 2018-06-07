10f5aa985b2d2e069f54918fdea857ad58caf315
Jeffrey Russom <jeffreyrussom@gmail.com>
2018-06-07 00:04:25 -0500

    Fix lambda

--- build_lambda.js
+++ build_lambda.js
@@ -1,12 +1,13 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
-const zlib = require('zlib');
+const path = require('path');
+const os = require('os');
 
 const s3 = new AWS.S3();
 const AdmZip = require('adm-zip');
-
+const mime = require('mime-types');
 const buildBucket = 'imgterestbuild';
-const deployBucket = '';
+const deployBucket = 'imgterest';
 
 var artifactKey = 'imgterestbuild.zip';
 
@@ -14,41 +15,31 @@ s3.getObject({ Bucket: buildBucket, Key: artifactKey }, function(err, data) {
 	if (err) {
 		console.log(err);
 	} else {
-		fs.writeFile(`./tmp/${artifactKey}`, data.Body, err => {
-			if (err) {
-				console.log(err);
-			} else {
-				const zip = new AdmZip(artifactKey);
-				const zipEntries = zip.getEntries();
-				zipEntries.forEach(function(zipEntry) {
-					console.log(zipEntry.entryName); // outputs zip entries information
-					const params = {
-						Body: zipEntry.getData(),
-						Bucket: 'imgterest',
-						ACL: 'public-read',
-						Key: zipEntry.entryName
-					};
-					s3.putObject(params, err => {
-						if (err) {
-							console.error(err);
-							return;
-						}
-						console.log('success!');
+		fs.mkdtemp(os.tmpdir(), (err, folder) => {
+			fs.writeFile(path.join(folder, artifactKey), data.Body, err => {
+				if (err) {
+					console.error(err);
+				} else {
+					const zip = new AdmZip(path.join(folder, artifactKey));
+					const zipEntries = zip.getEntries();
+					zipEntries.forEach(function(zipEntry) {
+						const params = {
+							Body: zipEntry.getData(),
+							Bucket: deployBucket,
+							ACL: 'public-read',
+							Key: zipEntry.entryName,
+							ContentType: mime.lookup(zipEntry.entryName)
+						};
+						s3.putObject(params, err => {
+							if (err) {
+								console.error(err);
+							} else {
+								console.log(`Uploaded ${zipEntry.entryName}`);
+							}
+						});
 					});
-					// console.log(zipEntry.getData().toString('utf8'));
-				});
-				s3.putObject();
-				// zip.extractAllTo('./tmp/', true);
-			}
+				}
+			});
 		});
-		// params = { Bucket: myBucket, Key: myKey, Body: 'Hello!' };
-
-		// s3.putObject(params, function(err, data) {
-		// 	if (err) {
-		// 		console.log(err);
-		// 	} else {
-		// 		console.log('Successfully uploaded data to myBucket/myKey');
-		// 	}
-		// });
 	}
 });
