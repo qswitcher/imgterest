9573070831a784edd85573becefe07b4057e7738
Jeffrey Russom <jeffreyrussom@gmail.com>
2018-06-06 23:15:31 -0500

    Update buildspec.yml

--- build_lambda.js
+++ build_lambda.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const zlib = require('zlib');
 
 const s3 = new AWS.S3();
+const AdmZip = require('adm-zip');
 
 const buildBucket = 'imgterestbuild';
 const deployBucket = '';
@@ -13,18 +14,31 @@ s3.getObject({ Bucket: buildBucket, Key: artifactKey }, function(err, data) {
 	if (err) {
 		console.log(err);
 	} else {
-		const unzip = zlib.createUnzip();
-
-		fs.writeFile(artifactKey, data.Body, err => {
+		fs.writeFile(`./tmp/${artifactKey}`, data.Body, err => {
 			if (err) {
 				console.log(err);
 			} else {
-				fs
-					.createReadStream(artifactKey)
-					.pipe(unzip)
-					.pipe(fs.createWriteStream('tmp'));
-
-				console.log('success!');
+				const zip = new AdmZip(artifactKey);
+				const zipEntries = zip.getEntries();
+				zipEntries.forEach(function(zipEntry) {
+					console.log(zipEntry.entryName); // outputs zip entries information
+					const params = {
+						Body: zipEntry.getData(),
+						Bucket: 'imgterest',
+						ACL: 'public-read',
+						Key: zipEntry.entryName
+					};
+					s3.putObject(params, err => {
+						if (err) {
+							console.error(err);
+							return;
+						}
+						console.log('success!');
+					});
+					// console.log(zipEntry.getData().toString('utf8'));
+				});
+				s3.putObject();
+				// zip.extractAllTo('./tmp/', true);
 			}
 		});
 		// params = { Bucket: myBucket, Key: myKey, Body: 'Hello!' };
